7005c294bf6274f1bbb46ce08c91d59029ad45a5
Mikolaj Olejnik <olejnikmikolaj@gmail.com>
2021-05-20 19:57:39 +0200

    commented code of multiplying

--- func.asm
+++ func.asm
@@ -1,35 +1,204 @@
 section .text
 global smul
-
 smul:
-    ; PROLOGUE
+    ;====PROLOGUE====
 	push    ebp
     mov     ebp, esp
 
-    sub     esp, 8
+    sub     esp, 48
 
     push    ebx
     push    esi
     push    edi
 
-    mov     eax, [ebp+8]    ; eax - result number
-    mov     esi, [ebp+12]   ; esi - first string
-    mov     edi, [ebp+16]   ; edi - second string
+    mov     eax, [ebp+8]        ; eax - result number
+    mov     esi, [ebp+12]       ; esi - first number
+    mov     edi, [ebp+16]       ; edi - second number
 
-    push    esi             ; push address of first string to get length
-    call    mystrlen        ; call the function
+    ;====PROGRAM====
+    push    esi                 ; push address of first string to get length
+    call    strlen              ; call the function
     add     esp, 4
-    mov     [ebp-4], eax    ; [ebp-4] - lenght of first string
+    mov     [ebp-4], eax        ; [ebp-4] - lenght of first number
 
-    push    edi             ; push address of first string to get length
-    call    mystrlen        ; call the function
+    push    edi                 ; push address of second string to get length
+    call    strlen              ; call the function
     add     esp, 4
-    mov     [ebp-8], eax    ; [ebp-8] - lenght of first string
+    mov     [ebp-8], eax        ; [ebp-8] - length of second number
 
-    mov     eax, [ebp-4]
-    add     eax, [ebp-8]
+    mov     ebx, [ebp-8]
+    add     ebx, [ebp-4]
+    mov     [ebp-12], ebx       ; [ebp-12] - length of result string
 
-    ; EPILOGUE
+    mov     dword [ebp-16], 0   ; [ebp-16] - incremented index (int a in C)
+                                ; [ebp-20] - index in result (int index in C)
+
+                                ; [ebp-24] - carry
+                                ; [ebp-28] - current digit from first number
+                                ; [ebp-32] - current digit from second number
+                                ; [ebp-36] - outer loop iterator
+                                ; [ebp-40] - inner loop iterator
+                                ; [ebp-44] - result of multiply with carry (sum in C)
+
+; filling result string with '0'
+
+    mov     dword [ebp-48], 0   ; [ebp-48] - fill string with '0' loop iterator
+    jmp     fill_loop
+zero:
+    mov     edx, [ebp-48]
+    mov     eax, [ebp+8]
+    add     eax, edx
+    mov     byte [eax], '0'
+    inc     dword [ebp-48]
+fill_loop:
+    mov     eax, [ebp-48]       ; eax - iterator
+    cmp     eax, [ebp-12]       ; string length
+    jl      zero                ; jump if eax < length 
+    mov     edx, [ebp-12]
+    mov     eax, [ebp+8]
+    add     eax, edx
+    mov     byte [eax], 0       ; set \0 at string's end
+
+; multiplying
+
+    ; for (int i=len1-1; i>=0; i--)
+    mov     eax, [ebp-4]        ; eax - first string length
+    dec     eax                 ; eax -= 1 (bo indexy od 0)
+    mov     [ebp-36], eax       ; outer loop iterator = eax
+    jmp     multiply
+outer_loop:
+    ; index = a
+    mov     eax, [ebp-16]       ; eax = a
+    mov     [ebp-20], eax       ; index = eax
+    ; carry = 0 in C
+    mov     dword [ebp-24], 0   ; carry = 0
+    ; n1 = num1[i] - '0'
+    mov     edx, [ebp-36]       ; edx = outer loop iterator (i in C)
+    mov     eax, esi            ; eax = first number address
+    add     eax, edx            ; eax += edx (eax = address of current char in string)
+    movzx   eax, byte [eax]     ; eax = current char in first string
+    sub     eax, '0'            ; eax -= '0' (ascii to int)
+    mov     [ebp-28], eax       ; current digit from first number = eax
+    ; for (int j=len2-1; j>=0; j--)
+    mov     eax, [ebp-8]        ; eax - first string length
+    dec     eax                 ; eax -= 1 (bo indexy od 0)
+    mov     [ebp-40], eax       ; inner loop iterator = eax
+    jmp     add_carry
+mul_digits:
+    ; n2 = num2[j] - '0'
+    mov     edx, [ebp-40]
+    mov     eax, edi
+    add     eax, edx
+    movzx   eax, byte [eax]     ; eax = current char in second string
+    sub     eax, '0'
+    mov     [ebp-32], eax
+    ; sum = n1*n2
+    mov     eax, [ebp-28]
+    imul    eax, [ebp-32]
+    mov     [ebp-44], eax
+    ; sum += result_string[index]
+    mov     edx, [ebp-20]       ; edx = index in result
+    mov     eax, [ebp+8]        ; eax = address of result string
+    add     eax, edx            ; eax = address of current char in result string
+    movzx   eax, byte [eax]     ; eax = current char in result string
+    add     [ebp-44], eax       ; sum += eax
+    ; sum += carry
+    mov     eax, [ebp-24]       ; eax = carry
+    add     [ebp-44], eax       ; sum += eax
+    ; sum -= '0'
+    sub     dword [ebp-44], '0'       ; sum -= '0' (ascii to int)
+    ; carry = sum/10 (*0x66666667/2^31/2^2 -> 0x66666667 ~= 2^33/5 -> 2^33/5/2^31/2^2 -> 1/10)
+    ;mov     ecx, [ebp-44]       ; ecx = sum
+    ;mov     edx, 0x66666667     ; edx = 0x66666667 ~= 2^33/5
+    ;mov     eax, ecx            ; eax = ecx = sum
+    ;mul     edx                 ; edx *= eax
+    ;sar     edx, 2              ; edx /= 2^2
+    ;mov     eax, ecx            ; eax = ecx = sum
+    ;sar     eax, 31             ; eax /= 2^31 
+    ;sub     edx, eax            ; edx -= eax
+    ;mov     eax, edx            ; eax = edx
+    ;mov     [ebp-24], eax       ; carry = eax
+
+    mov     eax, [ebp-44]
+    mov     edx, 0
+    mov     ebx, 10
+    div     ebx
+    mov     [ebp-24], eax
+    ;mov     ecx, [ebp-20]
+    ;mov     eax, [ebp+8]
+    ;add     eax, ecx
+    ;mov     eax, edx
+
+    ; result_string[index] = sum % 10
+    ;mov     ecx, [ebp-44]
+    ;mov     edx, 0x66666667
+    ;mov     eax, ecx
+    ;mul     edx
+    ;sar     edx, 2
+    ;mov     eax, ecx
+    ;sar     eax, 31
+    ;sub     edx, eax
+    ;mov     eax, edx
+    ;sal     eax, 2
+    ;add     eax, edx
+    ;add     eax, eax
+    ;sub     ecx, eax
+    ;mov     edx, ecx
+    mov     ecx, [ebp-20]
+    mov     eax, [ebp+8]
+    add     eax, ecx
+    mov     byte [eax], dl
+    ; result_string[index] += '0'
+    mov     edx, [ebp-20]
+    mov     eax, [ebp+8]
+    add     eax, edx
+    movzx   eax, byte [eax]
+    lea     ecx, [eax+48]
+    mov     edx, [ebp-20]
+    mov     eax, [ebp+8]
+    add     eax, edx
+    mov     edx, ecx
+    mov     byte [eax], dl
+    ; index++
+    inc     dword [ebp-20]
+
+    dec     dword [ebp-40]
+add_carry:
+    cmp     dword [ebp-40], 0
+    jns     mul_digits
+    ; result_string[index] += carry
+    mov     edx, [ebp-20]
+    mov     eax, DWORD [ebp+8]
+    add     eax, edx
+    movzx   eax, byte [eax]
+    mov     edx, eax
+    mov     eax, DWORD [ebp-24]
+    lea     ecx, [edx+eax]
+    mov     edx, DWORD [ebp-20]
+    mov     eax, DWORD [ebp+8]
+    add     eax, edx
+    mov     edx, ecx
+    mov     byte [eax], dl
+
+    ;a++
+    inc     dword [ebp-16]
+    ;index++
+    inc     dword [ebp-20]
+
+    dec     dword [ebp-36]
+multiply:
+    cmp     dword [ebp-36], 0
+    jns     outer_loop
+
+    mov     eax, [ebp+8]
+
+
+; removing zeros from end
+
+; reverse string
+
+    ;====EPILOGUE====
+
     pop     edi
     pop     esi
     pop     ebx
@@ -39,7 +208,7 @@ smul:
     ret
 
 
-mystrlen:
+strlen:
     push    ebp
     mov     ebp, esp
 
--- main.c
+++ main.c
@@ -3,61 +3,82 @@
 #include <stdlib.h>
 #include "func.h"
 
-
 char* multiply(char* result_string, char* num1, char* num2)
 {
     int len1 = strlen(num1);
     int len2 = strlen(num2);
     int result_len = len1+len2;
-
-    char result[result_len];
+    
+    // fill string with '0'
     for (int i=0; i<result_len; i++)
     {
-        result[i] = '0';
+        result_string[i] = '0';
     }
-    result[result_len] = '\0';
- 
-    int i_n1 = 0;
-    int i_n2 = 0;
+    result_string[result_len] = '\0';
+    
+    // multiply strings
+    int a=0;
+    int index;
+    int carry;
+    int n1;
+    int n2;
+    int sum;
      
     for (int i=len1-1; i>=0; i--)
     {
-        int carry = 0;
-        int n1 = num1[i] - '0';
- 
-        i_n2 = 0;
+        index = a;
+        carry = 0;
+        n1 = num1[i] - '0';
        
         for (int j=len2-1; j>=0; j--)
         {
-            int n2 = num2[j] - '0';
-            int sum = n1*n2 + result[i_n1 + i_n2] - 48 + carry;
+            n2 = num2[j] - '0';
+
+            sum = n1*n2;
+            sum += result_string[index];
+            sum += carry;
+            sum -= '0';
+
             carry = sum/10;
-            result[i_n1 + i_n2] = sum % 10 + 48;
-            i_n2++;
+
+            result_string[index] = sum % 10;
+            result_string[index] += '0';
+
+            index++;
         }
- 
-        if (carry > 0)
-            result[i_n1 + i_n2] += carry;
- 
-        i_n1++;
+        result_string[index] += carry;
+
+        a++;
+        index++;
     }
 
-    int i = result_len-1;
-    while (i>=0)
+    // remove '0' from sufix
+    int n = result_len-1;
+    while (n>=0)
     {
-        if (result[i] != '0')
+        if (result_string[n] != '0')
         {
-            int j = 0;
-            while (i>=0)
-            {
-                result_string[j] = result[i];
-                i--;
-                j++;
-            }
-            result_string[j] = '\0';
+            result_string[n+1] = '\0';
             break;
         }
-        i--;
+        n--;
+    }
+
+    // revers digits
+    int i = 0;
+    n = strlen(result_string)-1;
+
+    while(i<n)
+    {
+        char n_char = result_string[n];
+        if (n_char != '0')
+        {
+            char ch = result_string[i];
+            result_string[i] = n_char;
+            result_string[n] = ch;
+            i++;
+        }
+        n--;
     }
 
     return result_string;
@@ -65,8 +86,8 @@ char* multiply(char* result_string, char* num1, char* num2)
 
 int main()
 {
-	char* num1 = "7628346928349234";
-    char* num2 = "789236498234022029387402";
+	char* num1 = "12";
+    char* num2 = "13";
     int num1_len = strlen(num1);
     int num2_len = strlen(num2);
 
@@ -81,10 +102,11 @@ int main()
         char result_string[result_len];   
 
 
-        //strcpy(result_string, smul(result_string, num1, num2));
-        int result = smul(result_string, num1, num2);
-        //puts(result_string);
-        printf("%i", result);
+        strcpy(result_string, smul(result_string, num1, num2));
+        puts(result_string);
+
+        //int result = smul(result_string, num1, num2);
+        //printf("%i\n", result);
     }
 
     return 0;
